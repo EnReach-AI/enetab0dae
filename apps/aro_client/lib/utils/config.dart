62a84c59cacff0b1dedc3119a04f17dc1e57c828
Colton <lzymelo@proton.me>
2026-02-10 18:01:28 +0800

    [all] Fix

--- apps/aro_client/lib/utils/config.dart
+++ apps/aro_client/lib/utils/config.dart
@@ -1,9 +1,9 @@
 class AllConfig {
   static const apiBase = "https://staging-api.aro.network";
   static const ws = "staging-ws.aro.network";
-  // static const deskTopURL =
-  //     "https://0ee63895-262b.ipproxy.aro.network/desktop/";
-  static const deskTopURL = "http://192.168.2.10:3000";
-  // static const mobileURL = "https://0ee63895-262b.ipproxy.aro.network/mobile/";
-  static const mobileURL = "http://192.168.2.10:3000";
+  static const deskTopURL =
+      "https://0ee63895-262b.ipproxy.aro.network/desktop/";
+  // static const deskTopURL = "http://192.168.2.10:3000";
+  static const mobileURL = "https://0ee63895-262b.ipproxy.aro.network/mobile/";
+  // static const mobileURL = "http://192.168.2.10:3000";
 }
