78fbb14cec75199be2955add2e49fb2850685e3c
Colton <lzymelo@proton.me>
2026-02-27 21:56:26 +0800

    [all] Fic

--- scripts/install_aro_linux_service.sh
+++ scripts/install_aro_linux_service.sh
@@ -6,10 +6,10 @@ TARGET_SERVICE="/etc/systemd/system/aro_linux.service"
 
 if [ "$(uname -s)" = "Linux" ]; then
     echo "🔄 Installing systemd service..."
-    sudo cp "$SERVICE_FILE" "$TARGET_SERVICE"
-    sudo systemctl daemon-reload
-    sudo systemctl enable aro_linux.service
-    sudo systemctl start aro_linux.service
+     cp "$SERVICE_FILE" "$TARGET_SERVICE"
+     systemctl daemon-reload
+     systemctl enable aro_linux.service
+     systemctl start aro_linux.service
     echo "✅ systemd service installed and started."
 else
     echo "❌ Systemd service installation is not supported on this platform."
